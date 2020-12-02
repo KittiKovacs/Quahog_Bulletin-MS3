78d75977936af1f1a33810545397a0860228e925
Kitti Kovacs <bambula86@gmail.com>
2020-12-02 19:45:26 +0000

    add login and register functionality and templates

--- app.py
+++ app.py
@@ -26,13 +26,67 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        register = {
+            "first_name": request.form.get("first_name").lower(),
+            "last_name": request.form.get("last_name").lower(),
+            "house_no": request.form.get("house_number"),
+            "street_name":  request.form.get("street").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    else:
+        flash("Incorrect Username and/or Password")
+        return redirect(url_for("login"))
+
     return render_template('login.html')
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    
+    if session ["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route('/boards')
+def boards():
+     return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
