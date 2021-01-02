8188f550042ef711e1f277d94b39442a3d576f33
Kitti Kovacs <bambula86@gmail.com>
2021-01-02 17:33:45 +0000

    Add a custom 404 error page

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ def index():
     return render_template("index.html", categories=categories)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 # User authentication
 
 
